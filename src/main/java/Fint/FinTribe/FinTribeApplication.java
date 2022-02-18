2788a2aa578e1e45bca0805dae9fb4eefdab9ff8
KJI-DEC <kji_dec22@ewhain.net>
2022-02-18 16:15:05 +0900

    Add: 실행 및 테스트 코드 추가

---
+++ src/main/java/Fint/FinTribe/FinTribeApplication.java
@@ -0,0 +1,13 @@
+package Fint.FinTribe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinTribeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinTribeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Fint/FinTribe/FinTribeApplicationTests.java
@@ -0,0 +1,13 @@
+package Fint.FinTribe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinTribeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
