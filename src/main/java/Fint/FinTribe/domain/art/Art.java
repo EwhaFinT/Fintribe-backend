2e5c6a39ac69996fe9a7b5731eb88f09f709252d
KimYujeong <70785620+kimyu0218@users.noreply.github.com>
2022-02-19 18:19:02 +0900

    Add: Art Entity

---
+++ src/main/java/Fint/FinTribe/domain/art/Art.java
@@ -0,0 +1,21 @@
+package Fint.FinTribe.domain.art;
+
+import lombok.*;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Getter
+@Builder
+@Document(collection = "art")
+public class Art {
+    private Long artId;
+    private String painter;
+    private String artName;
+    private String detail;
+    private Long price;
+    private String nftAdd;
+    private String paint;
+    private Boolean sold = false;
+    private List<Long> userId;
+}
