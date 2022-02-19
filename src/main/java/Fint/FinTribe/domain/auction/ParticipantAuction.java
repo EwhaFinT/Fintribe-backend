a59b0b7f1bb795ef8f49b74567f9f4a5cba0dc37
KimYujeong <70785620+kimyu0218@users.noreply.github.com>
2022-02-19 18:35:53 +0900

    Add: ParticipantAuction Entity

--- src/main/java/Fint/FinTribe/domain/auction/ParticipantAuction.java
+++ src/main/java/Fint/FinTribe/domain/auction/ParticipantAuction.java
@@ -1,2 +1,12 @@
-package Fint.FinTribe.domain.auction;public class ParticipantAuction {
+package Fint.FinTribe.domain.auction;
+
+import lombok.*;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@Builder
+@Document(collection = "participant_auction")
+public class ParticipantAuction {
+    private Long userId;
+    private Double ratio;
 }
