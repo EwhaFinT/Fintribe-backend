e5df32e18cbc5a495105721efe3271f2fafbd36e
KimYujeong <70785620+kimyu0218@users.noreply.github.com>
2022-02-19 18:37:01 +0900

    Add: Price Entity

--- src/main/java/Fint/FinTribe/domain/auction/Price.java
+++ src/main/java/Fint/FinTribe/domain/auction/Price.java
@@ -1,2 +1,16 @@
-package Fint.FinTribe.domain.auction;public class Price {
+package Fint.FinTribe.domain.auction;
+
+import lombok.*;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Getter
+@Builder
+@Document(collection = "price")
+public class Price {
+    private Long priceId;
+    private Long auctionPrice;
+    private Double remainderRatio;
+    private List<ParticipantAuction> participantAuction;
 }
