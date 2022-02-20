8f70129c5a0cd0efc7fce12489baad1556a88f30
KimYujeong <70785620+kimyu0218@users.noreply.github.com>
2022-02-20 16:47:56 +0900

    Fix: Price Entity 필드 수정

--- src/main/java/Fint/FinTribe/domain/auction/Price.java
+++ src/main/java/Fint/FinTribe/domain/auction/Price.java
@@ -3,8 +3,6 @@ package Fint.FinTribe.domain.auction;
 import lombok.*;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.util.List;
-
 @Getter
 @Builder
 @Document(collection = "price")
@@ -12,5 +10,4 @@ public class Price {
     private Long priceId;
     private Long auctionPrice;
     private Double remainderRatio;
-    private List<ParticipantAuction> participantAuction;
 }
