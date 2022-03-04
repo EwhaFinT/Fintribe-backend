88d7d77dbd50e2ded907e64af1fd5a69d5e9cfb9
KJI-DEC <kji_dec22@ewhain.net>
2022-03-04 22:56:55 +0900

    Fix: 자료형 및 builder/생성자 수정

--- src/main/java/Fint/FinTribe/domain/community/Vote.java
+++ src/main/java/Fint/FinTribe/domain/community/Vote.java
@@ -8,16 +8,18 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
-@Builder
+@AllArgsConstructor
 @Document(collection = "vote")
 public class Vote {
+    private Integer voteId;
     private ObjectId userId;
+    private String identity;
     private String title;
     private Double resalePrice;
     private LocalDateTime startTime;
     private LocalDateTime endTime;
     private Boolean isDeleted;
-    private Float agreement;
-    private Float disagreement;
+    private Double agreement;
+    private Double disagreement;
     private List<ParticipantVote> participants;
 }
