05255fbbb4070fac8e3736a6a53924434bffeb66
KJI-DEC <kji_dec22@ewhain.net>
2022-03-04 22:57:02 +0900

    Fix: 자료형 및 builder/생성자 수정

--- src/main/java/Fint/FinTribe/domain/community/ParticipantVote.java
+++ src/main/java/Fint/FinTribe/domain/community/ParticipantVote.java
@@ -7,11 +7,11 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.time.LocalDateTime;
 
 @Getter
-@Builder
+@AllArgsConstructor
 @Document(collection = "participant_vote")
 public class ParticipantVote {
     private ObjectId userId;
     private Boolean choice;
     private LocalDateTime participatedAt;
-    private Float ratio;
+    private Double ratio;
 }
