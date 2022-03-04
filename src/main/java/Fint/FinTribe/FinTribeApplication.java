aa4ab0222b3a43e67509f2986b7c92dd65aa9bf8
KimYujeong <70785620+kimyu0218@users.noreply.github.com>
2022-03-04 23:02:13 +0900

    develop

--- src/main/java/Fint/FinTribe/FinTribeApplication.java
+++ src/main/java/Fint/FinTribe/FinTribeApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 @EnableMongoRepositories({"Fint.FinTribe.domain"})
+@EnableScheduling
 public class FinTribeApplication {
 
 	public static void main(String[] args) {
