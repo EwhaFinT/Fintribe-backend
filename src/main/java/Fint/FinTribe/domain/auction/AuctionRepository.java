ad912249be9aa2a72808b736154f1a38b2967a97
KimYujeong <70785620+kimyu0218@users.noreply.github.com>
2022-03-05 19:44:42 +0900

    Git 브랜치 분리

--- src/main/java/Fint/FinTribe/domain/auction/AuctionRepository.java
+++
@@ -1,8 +0,0 @@
-package Fint.FinTribe.domain.auction;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AuctionRepository extends MongoRepository<Auction, Object> {
-}
