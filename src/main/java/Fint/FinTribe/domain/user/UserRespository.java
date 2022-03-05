d13c31f44ae3c57e4f9307aa4041a9c56a58ae1e
KimYujeong <70785620+kimyu0218@users.noreply.github.com>
2022-03-05 19:32:37 +0900

    Fix: Repository Type 수정 (Object -> ObjectId)

--- src/main/java/Fint/FinTribe/domain/user/UserRespository.java
+++ src/main/java/Fint/FinTribe/domain/user/UserRespository.java
@@ -1,5 +1,6 @@
 package Fint.FinTribe.domain.user;
 
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -7,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface UserRespository extends MongoRepository<User, Object> {
+public interface UserRespository extends MongoRepository<User, ObjectId> {
     @Query("{'identity': ?0}")
     Optional<User> findByIdentity(String identity);
 }
