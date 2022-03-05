125c76ce856bf56ad1fac843d449a52c6f2037cd
KimYujeong <70785620+kimyu0218@users.noreply.github.com>
2022-03-05 12:05:06 +0900

    Fix: @Id 어노테이션 추가

--- src/main/java/Fint/FinTribe/domain/auctionDate/AuctionDate.java
+++ src/main/java/Fint/FinTribe/domain/auctionDate/AuctionDate.java
@@ -2,6 +2,7 @@ package Fint.FinTribe.domain.auctionDate;
 
 import lombok.*;
 import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
@@ -11,6 +12,7 @@ import java.util.List;
 @Builder
 @Document(collection = "auction_date")
 public class AuctionDate {
+    @Id
     private ObjectId auctionDateId;
     private LocalDateTime auctionDate;
     private List<ObjectId> artId;
