b627475fc7c8881f5356ef930c9d2f31ea648733
KimYujeong <70785620+kimyu0218@users.noreply.github.com>
2022-03-05 19:34:41 +0900

    Fix: Repository Type 수정 (Object -> ObjectId)

--- src/main/java/Fint/FinTribe/domain/auction/AuctionRepository.java
+++ src/main/java/Fint/FinTribe/domain/auction/AuctionRepository.java
@@ -1,8 +1,9 @@
 package Fint.FinTribe.domain.auction;
 
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AuctionRepository extends MongoRepository<Auction, Object> {
+public interface AuctionRepository extends MongoRepository<Auction, ObjectId> {
 }
