386790914dc97366b3a39b5caabdaaf72e83ebb2
KimYujeong <70785620+kimyu0218@users.noreply.github.com>
2022-03-05 12:04:12 +0900

    Add: ResaleDate Entity

--- src/main/java/Fint/FinTribe/domain/resaleDate/ResaleDate.java
+++ src/main/java/Fint/FinTribe/domain/resaleDate/ResaleDate.java
@@ -1,2 +1,19 @@
-package Fint.FinTribe.domain.resaleDate;public class ResaleDate {
+package Fint.FinTribe.domain.resaleDate;
+
+import lombok.*;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Builder
+@Document(collection = "resale_date")
+public class ResaleDate {
+    @Id
+    private ObjectId resaleDateId;
+    private LocalDateTime resaleDate;
+    private List<ObjectId> artId;
 }
