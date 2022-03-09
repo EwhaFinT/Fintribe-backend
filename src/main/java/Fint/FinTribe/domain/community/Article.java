e7f2ffb22352e990a0f355dd11ef2cefcd684de2
KJI-DEC <kji_dec22@ewhain.net>
2022-03-09 23:01:14 +0900

    Fix: entity 수정

--- src/main/java/Fint/FinTribe/domain/community/Article.java
+++ src/main/java/Fint/FinTribe/domain/community/Article.java
@@ -2,16 +2,19 @@ package Fint.FinTribe.domain.community;
 
 import lombok.*;
 import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Getter
-@AllArgsConstructor
+@Setter
+@Builder
 @Document(collection = "article")
 public class Article {
-    private Long articleId;
+    @Id
+    private ObjectId articleId;
+    private ObjectId communityId;
     private ObjectId userId;
     private String identity;
     private String title;
@@ -19,6 +22,4 @@ public class Article {
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
     private Boolean isDeleted;
-    private List<Comment> comments;
-    private List<ReComment> reComments;
 }
