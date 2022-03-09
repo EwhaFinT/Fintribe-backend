22a518704aed82ccaafcea6a9881561b6e839d9e
KJI-DEC <kji_dec22@ewhain.net>
2022-03-10 04:10:44 +0900

    Feat: findByIdAndArticleId 추가

--- src/main/java/Fint/FinTribe/domain/community/CommentRepository.java
+++ src/main/java/Fint/FinTribe/domain/community/CommentRepository.java
@@ -5,8 +5,12 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CommentRepository extends MongoRepository<Comment, Integer> {
     @Query("{'articleId': ?0}")
     List<Comment> findByArticleId(ObjectId articleId);
+
+    @Query("{$and:[{'_id': ?0}, {'articleId': ?0}]")
+    Optional<Comment> findByIdAndArticleId(Integer commentId, ObjectId articleId);
 }
