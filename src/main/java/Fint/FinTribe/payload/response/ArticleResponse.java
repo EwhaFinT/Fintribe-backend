70a9dc6731090c1a6ecf4e66f077bed7e8be328e
KJI-DEC <kji_dec22@ewhain.net>
2022-03-09 23:16:18 +0900

    Add: ArticleResponse 추가

---
+++ src/main/java/Fint/FinTribe/payload/response/ArticleResponse.java
@@ -0,0 +1,9 @@
+package Fint.FinTribe.payload.response;
+
+import lombok.AllArgsConstructor;
+import org.bson.types.ObjectId;
+
+@AllArgsConstructor
+public class ArticleResponse {
+    private String message;
+}
