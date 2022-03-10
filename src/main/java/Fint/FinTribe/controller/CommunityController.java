8cc2009bc5509a682e89aa4b216cad59e42ed9c2
KJI-DEC <kji_dec22@ewhain.net>
2022-03-11 00:50:19 +0900

    Fix: 함수명 변경, 변경 api 적용

--- src/main/java/Fint/FinTribe/controller/CommunityController.java
+++ src/main/java/Fint/FinTribe/controller/CommunityController.java
@@ -2,6 +2,7 @@ package Fint.FinTribe.controller;
 
 import Fint.FinTribe.domain.community.Community;
 import Fint.FinTribe.payload.request.*;
+import Fint.FinTribe.payload.response.ArticleResponse;
 import Fint.FinTribe.payload.response.CommunityResponse;
 import Fint.FinTribe.payload.response.VoteCheckResponse;
 import Fint.FinTribe.service.community.CommunityService;
@@ -22,67 +23,65 @@ public class CommunityController {
 
     //커뮤니티
     @GetMapping("/community")
-    public ResponseEntity<?> getCommunity(@RequestParam @Valid ObjectId artId, @RequestParam @Valid ObjectId userId){
-        CommunityResponse communityResponse = communityService.getCommunityInformation(artId, userId);
+    public ResponseEntity<?> getCommunityInformation(@RequestParam @Valid ObjectId artId, @RequestParam @Valid ObjectId userId){
+        CommunityResponse communityResponse = communityService.getCommunity(artId, userId);
         return new ResponseEntity<>(communityResponse, HttpStatus.OK);
     }
 
     //게시글
     @PostMapping("/article")
-    public ResponseEntity<?> createArticle(@RequestBody @Valid ArticleRequest articleRequest){
-        communityService.createArticle(articleRequest);
-        return new ResponseEntity<>(HttpStatus.OK);
+    public ResponseEntity<?> postArticle(@RequestBody @Valid ArticleRequest articleRequest){
+        ArticleResponse articleResponse = communityService.createArticle(articleRequest);
+        return new ResponseEntity<>(articleResponse, HttpStatus.OK);
     }
 
     @PostMapping("/revise-article")
     public ResponseEntity<?> patchArticle(@RequestBody @Valid ReviseArticleRequest articleRequest){
-        communityService.patchArticle(articleRequest);
-        return new ResponseEntity<>(HttpStatus.OK);
+        ArticleResponse articleResponse = communityService.updateArticle(articleRequest);
+        return new ResponseEntity<>(articleResponse, HttpStatus.OK);
     }
 
     @DeleteMapping("/delete-article")
-    public ResponseEntity<?> deleteArticle(@RequestParam @Valid Long articleId, @RequestParam @Valid ObjectId communityId){
-        communityService.deleteArticle(articleId, communityId);
-        return new ResponseEntity<>(HttpStatus.OK);
+    public ResponseEntity<?> deleteArticle(@RequestParam @Valid ObjectId articleId){
+        ArticleResponse articleResponse = communityService.deleteArticle(articleId);
+        return new ResponseEntity<>(articleResponse, HttpStatus.OK);
     }
 
     //댓글
     @PostMapping("/comment")
-    public ResponseEntity<?> createComment(@RequestBody @Valid CommentRequest commentRequest){
+    public ResponseEntity<?> postComment(@RequestBody @Valid CommentRequest commentRequest){
         communityService.createComment(commentRequest);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @PostMapping("/revise-comment")
     public ResponseEntity<?> patchComment(@RequestBody @Valid ReviseCommentRequest commentRequest){
-        communityService.patchComment(commentRequest);
+        communityService.updateComment(commentRequest);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @DeleteMapping("/delete-comment")
-    public ResponseEntity<?> deleteComment(@RequestParam @Valid Long articleId, @RequestParam @Valid List<Integer> commentId, @RequestParam @Valid ObjectId communityId, @RequestParam @Valid List<Integer> tagCommentId){
-        communityService.deleteComment(articleId, commentId, communityId, tagCommentId);
+    public ResponseEntity<?> deleteComment(@RequestParam @Valid ObjectId articleId, @RequestParam @Valid Integer commentId, @RequestParam @Valid Integer tagCommentId){
+        communityService.deleteComment(articleId, commentId, tagCommentId);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    //투표 생성
+    //투표
     @PostMapping("/vote-proposal")
-    public ResponseEntity<?> createVote(@RequestBody @Valid VoteProposalRequest voteRequest){
+    public ResponseEntity<?> postVote(@RequestBody @Valid VoteProposalRequest voteRequest){
         communityService.createVote(voteRequest);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    //투표 참여
     @PostMapping("/vote")
     public ResponseEntity<?> participateVote(@RequestBody @Valid VoteRequest voteRequest){
-        String result = communityService.participateVote(voteRequest);
-        return new ResponseEntity<>(result, HttpStatus.OK);
+        communityService.participateVote(voteRequest);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    //투표창 접속
     @GetMapping("/check")
-    public ResponseEntity<?> getVote(@RequestParam @Valid ObjectId communityId, @RequestParam @Valid Integer voteId){
-        VoteCheckResponse voteResponse = communityService.getVoteInformation(communityId, voteId);
+    public ResponseEntity<?> getVote(@RequestParam @Valid ObjectId voteId){
+        VoteCheckResponse voteResponse = communityService.getVoteInformation(voteId);
         return new ResponseEntity<>(voteResponse, HttpStatus.OK);
     }
 }
