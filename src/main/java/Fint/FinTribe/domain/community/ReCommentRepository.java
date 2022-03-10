9c97dd1ff636f2cf2c7045280d5de5f0caec4bd7
KJI-DEC <kji_dec22@ewhain.net>
2022-03-11 00:49:05 +0900

    Feat: 조회 함수 추가

--- src/main/java/Fint/FinTribe/domain/community/ReCommentRepository.java
+++ src/main/java/Fint/FinTribe/domain/community/ReCommentRepository.java
@@ -1,11 +1,16 @@
 package Fint.FinTribe.domain.community;
 
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
-public interface ReCommentRepository extends MongoRepository<Comment, Integer> {
+public interface ReCommentRepository extends MongoRepository<ReComment, Integer> {
     @Query("{'tagCommentId': ?0}")
     List<ReComment> findByTagCommentId(Integer commentId);
+
+    @Query("{$and:[{'_id': ?0}, {'tagCommentId': ?0}, {'articleId': ?0}]")
+    Optional<ReComment> findByIdAndTagCommentIdAndArticleId(Integer commentId, Integer tagCommentId, ObjectId articleId);
 }
