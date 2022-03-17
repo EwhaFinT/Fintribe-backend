859bca5e5347bcf904d0becb2b1f9aa978af90bc
KimYujeong <70785620+kimyu0218@users.noreply.github.com>
2022-03-17 22:19:03 +0900

    Develop Test

--- src/test/java/Fint/FinTribe/service/AuctionServiceTest.java
+++ src/test/java/Fint/FinTribe/service/AuctionServiceTest.java
@@ -1,30 +1,21 @@
 package Fint.FinTribe.service;
 
-import Fint.FinTribe.domain.art.Art;
 import Fint.FinTribe.domain.art.ArtRepository;
 import Fint.FinTribe.domain.auction.Auction;
-import Fint.FinTribe.domain.auction.AuctionRepository;
 import Fint.FinTribe.domain.auction.Price;
-import Fint.FinTribe.payload.request.NewPriceRequest;
-import Fint.FinTribe.payload.request.ParticipateAuctionRequest;
-import Fint.FinTribe.payload.request.PricelistRequest;
+import Fint.FinTribe.payload.request.*;
 import Fint.FinTribe.payload.response.PricelistResponse;
 import org.assertj.core.api.Assertions;
+import org.bson.types.ObjectId;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.repository.query.FluentQuery;
 
+import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
-import java.util.function.Function;
 
 @SpringBootTest
 class AuctionServiceTest {
@@ -35,85 +26,88 @@ class AuctionServiceTest {
     @Autowired
     private AuctionService auctionService;
 
-    /*
+    private List<Auction> auctions;
+
     @BeforeEach
     void setUp() {
-        artRepository.save(
-                Art.builder()
-                        .artId("artId").artName("artName")
-                        .paint("painter").detail("detail").price(50)
-                        .nftAdd(null).paint(null).userId(null).build()
-        );
-        auctionService.createSampleAuction();
+        LocalDate today = LocalDate.now();
+        auctionService.deleteAuctions();
+        auctionService.makeAuctions(today);
+        auctions = auctionService.getAuctions();
     }
 
     @AfterEach
-    void tearDown() { artRepository.deleteAll(); auctionService.deleteAll(); }
+    void tearDown() { /*artRepository.deleteAll(); auctionService.deleteAll();*/ }
+
+//    테스트용 userId : 62325e0c9ff0d44d0ed82bf0 6232749829304e715460713e
+//    테스트용 auctionId : 623329392862733ba84fc1d7
+//    테스트용 priceId : 62327a05640e17746283fb9c
 
     @Test
     @DisplayName("새로운 경매 가격 제안 성공 테스트")
-    void newPriceSuccess() {
-        NewPriceRequest newPriceRequest = new NewPriceRequest("userId-newPrice", "auctionId", 100, 0.5);
-        Assertions.assertThat(auctionService.newPrice(newPriceRequest));
+    void newPriceTransactionSuccess() {
+        Auction auction = auctions.get(0);
+        NewPriceTransactionRequest newPriceTransactionRequest = new NewPriceTransactionRequest(new ObjectId("62325e0c9ff0d44d0ed82bf0"), auction.getAuctionId(), 100, 1.0);
+        Assertions.assertThat(auctionService.newPrice(newPriceTransactionRequest));
     }
 
     @Test
-    @DisplayName("새로운 경매 가격 제안 성공 테스트")
-    void newPriceFail() {
-        NewPriceRequest newPriceRequest = new NewPriceRequest("userId-newPrice", "auctionId", 25, 0.5);
-        Assertions.assertThat(auctionService.newPrice(newPriceRequest));
+    @DisplayName("새로운 경매 가격 제안 실패 테스트")
+    void newPriceTransactionFail() {
+        Auction auction = auctions.get(0);
+        NewPriceTransactionRequest newPriceTransactionRequest = new NewPriceTransactionRequest(new ObjectId("62325e0c9ff0d44d0ed82bf0"), auction.getAuctionId(), 25, 0.5);
+        Assertions.assertThat(auctionService.newPrice(newPriceTransactionRequest));
     }
 
     @Test
-    @DisplayName("기존 경매 참여 성공 테스트 - 신규 회원")
+    @DisplayName("새로운 경매 가격 성공 테스트")
+    void newPrice() {
+        Auction auction = auctions.get(0);
+        NewPriceRequest newPriceRequest = new NewPriceRequest(new ObjectId("62325e0c9ff0d44d0ed82bf0"), auction.getAuctionId(), 100, 1.0, "rlp");
+        Assertions.assertThat(auctionService.newPriceSuccess(newPriceRequest));
+    }
+
+    @Test
+    @DisplayName("기존 경매 참여 성공 테스트 - 같은 회원")
     void participateAuctionSuccess1() {
-        NewPriceRequest newPriceRequest = new NewPriceRequest("userId-participant1", "auctionId", 150, 0.5);
-        Object priceId = auctionService.newPrice(newPriceRequest).getPriceId();
-        ParticipateAuctionRequest participateAuctionRequest = new ParticipateAuctionRequest("userId-participant2", priceId, "auctionId", 0.3);
-        Assertions.assertThat(auctionService.participateAuction(participateAuctionRequest).getPriceId()).isEqualTo(priceId);
+        ObjectId userId = new ObjectId("62325e0c9ff0d44d0ed82bf0");
+        ObjectId auctionId = new ObjectId("623329392862733ba84fc1d7");
+        NewPriceRequest newPriceRequest = new NewPriceRequest(userId, auctionId, 150, 0.5, "rlp");
+        ObjectId priceId = auctionService.newPriceSuccess(newPriceRequest).getPriceId();
+        ParticipateAuctionRequest participateAuctionRequest = new ParticipateAuctionRequest(userId, priceId, auctionId, 0.3, "rlp");
+        Assertions.assertThat(auctionService.participateAuctionSuccess(participateAuctionRequest).getPriceId()).isEqualTo(priceId);
     }
 
     @Test
-    @DisplayName("기존 경매 참여 성공 테스트 - 기존 회원")
+    @DisplayName("기존 경매 참여 성공 테스트 - 서로 다른 회원")
     void participateAuctionSuccess2() {
-        NewPriceRequest newPriceRequest = new NewPriceRequest("userId-participant1", "auctionId", 150, 0.5);
-        Object priceId = auctionService.newPrice(newPriceRequest).getPriceId();
-        ParticipateAuctionRequest participateAuctionRequest = new ParticipateAuctionRequest("userId-participant1", priceId, "auctionId", 0.3);
-        Assertions.assertThat(auctionService.participateAuction(participateAuctionRequest).getPriceId()).isEqualTo(priceId);
+        ObjectId userId1 = new ObjectId("62325e0c9ff0d44d0ed82bf0");
+        ObjectId userId2 = new ObjectId("6232749829304e715460713e");
+        ObjectId auctionId = new ObjectId("623329392862733ba84fc1d7");
+        NewPriceRequest newPriceRequest = new NewPriceRequest(userId1, auctionId, 150, 0.5, "rlp");
+        ObjectId priceId = auctionService.newPriceSuccess(newPriceRequest).getPriceId();
+        ParticipateAuctionRequest participateAuctionRequest = new ParticipateAuctionRequest(userId2, priceId, auctionId, 0.5, "rlp");
+        Assertions.assertThat(auctionService.participateAuctionSuccess(participateAuctionRequest).getPriceId()).isEqualTo(priceId);
     }
+
 
     @Test
     @DisplayName("기존 경매 참여 실패 테스트 - 잔여 지분 초과")
     void participateAuctionFail() {
-        NewPriceRequest newPriceRequest = new NewPriceRequest("userId-participantFail1", "auctionId", 200, 0.6);
-        Object priceId = auctionService.newPrice(newPriceRequest);
-        ParticipateAuctionRequest participateAuctionRequest = new ParticipateAuctionRequest("userId-participantFail2", priceId, "auctionId", 0.5);
-        Assertions.assertThat(auctionService.participateAuction(participateAuctionRequest).getPriceId()).isEqualTo(0);
-    }
-
-    @Test
-    @DisplayName("기존 경매 제안 리스트 & 상한가 가져오기 - 상한가 존재X")
-    void getPricelist1() {
-        auctionService.newPrice(new NewPriceRequest("userId1", "auctionId", 100, 0.5));
-        auctionService.newPrice(new NewPriceRequest("userId2", "auctionId", 150, 0.5));
-        auctionService.newPrice(new NewPriceRequest("userId3", "auctionId", 200, 0.4));
-        PricelistRequest pricelistRequest = new PricelistRequest("auctionId");
-        PricelistResponse pricelistResponse = auctionService.getPricelist(pricelistRequest);
-        List<Price> pricelist = pricelistResponse.getPricelist();
-        Assertions.assertThat(pricelistResponse.getPrice()).isNotEqualTo(pricelist.get(0).getAuctionPrice());
+        ObjectId userId = new ObjectId("62325e0c9ff0d44d0ed82bf0");
+        ObjectId auctionId = new ObjectId("623329392862733ba84fc1d7");
+        ObjectId priceId = new ObjectId("62327a05640e17746283fb9c");
+        ParticipateAuctionTransactionRequest participateAuctionTransactionRequest = new ParticipateAuctionTransactionRequest(userId, priceId, auctionId, 0.5);
+        Assertions.assertThat(auctionService.participateAuction(participateAuctionTransactionRequest).getTo()).isEqualTo(null);
     }
 
     @Test
     @DisplayName("기존 경매 제안 리스트 & 상한가 가져오기 - 상한가 존재")
-    void getPricelist2() {
-        auctionService.newPrice(new NewPriceRequest("userId1", "auctionId", 100, 0.5));
-        auctionService.newPrice(new NewPriceRequest("userId2", "auctionId", 150, 1.0));
-        auctionService.newPrice(new NewPriceRequest("userId3", "auctionId", 200, 0.4));
-        PricelistRequest pricelistRequest = new PricelistRequest("auctionId");
+    void getPricelist() {
+        ObjectId auctionId = new ObjectId("623329392862733ba84fc1d7");
+        PricelistRequest pricelistRequest = new PricelistRequest(auctionId);
         PricelistResponse pricelistResponse = auctionService.getPricelist(pricelistRequest);
         List<Price> pricelist = pricelistResponse.getPricelist();
         Assertions.assertThat(pricelistResponse.getPrice()).isNotEqualTo(pricelist.get(0).getAuctionPrice());
     }
-
-     */
 }
--- src/test/java/Fint/FinTribe/service/UserServiceTest.java
+++ src/test/java/Fint/FinTribe/service/UserServiceTest.java
@@ -3,6 +3,7 @@ package Fint.FinTribe.service;
 import Fint.FinTribe.payload.request.*;
 import Fint.FinTribe.payload.response.*;
 import org.assertj.core.api.Assertions;
+import org.bson.types.ObjectId;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -22,7 +23,6 @@ class UserServiceTest {
 
     @AfterEach
     void tearDown() {
-        userService.deleteAll();
     }
 
     @Test
@@ -45,8 +45,6 @@ class UserServiceTest {
     @Test
     @DisplayName("로그인 성공 테스트")
     void loginUserSuccess() {
-        SignupRequest signupRequest = new SignupRequest("identity", "password", "name", "010-1886-1886", "ewhafint@gmail.com");
-        userService.registerUser(signupRequest);
         LoginRequest loginRequest = new LoginRequest("identity", "password");
         LoginResponse loginResponse = userService.loginUser(loginRequest);
         Assertions.assertThat(loginResponse.getUserId()).isNotEqualTo(null);
@@ -63,8 +61,6 @@ class UserServiceTest {
     @Test
     @DisplayName("로그인 실패 테스트 - 비밀번호 불일치")
     void loginUserFail2() {
-        SignupRequest signupRequest = new SignupRequest("identity", "password", "name", "010-1886-1886", "ewhafint@gmail.com");
-        userService.registerUser(signupRequest);
         LoginRequest loginRequest = new LoginRequest("identity", "password2");
         LoginResponse loginResponse = userService.loginUser(loginRequest);
         Assertions.assertThat(loginResponse.getUserId()).isEqualTo(null);
@@ -73,8 +69,6 @@ class UserServiceTest {
     @Test
     @DisplayName("마이페이지 테스트")
     void myPage() {
-        SignupRequest signupRequest = new SignupRequest("identity", "password", "name", "010-1886-1886", "ewhafint@gmail.com");
-        userService.registerUser(signupRequest);
         LoginRequest loginRequest = new LoginRequest("identity", "password");
         MypageRequest mypageRequest = new MypageRequest(userService.loginUser(loginRequest).getUserId());
         MypageResponse mypageResponse = userService.myPage(mypageRequest);
@@ -84,8 +78,6 @@ class UserServiceTest {
     @Test
     @DisplayName("아이디 찾기 성공 테스트")
     void findIdSuccess() {
-        SignupRequest signupRequest = new SignupRequest("identity", "password", "name", "010-1886-1886", "ewhafint@gmail.com");
-        userService.registerUser(signupRequest);
         FindIdRequest findIdRequest = new FindIdRequest("name", "010-1886-1886");
         FindIdResponse findIdResponse = userService.findId(findIdRequest);
         System.out.println(findIdResponse.getIdentity());
@@ -95,8 +87,6 @@ class UserServiceTest {
     @Test
     @DisplayName("아이디 찾기 실패 테스트")
     void findIdFail() {
-        SignupRequest signupRequest = new SignupRequest("identity", "password", "name", "010-1886-1886", "ewhafint@gmail.com");
-        userService.registerUser(signupRequest);
         FindIdRequest findIdRequest = new FindIdRequest("name_", "010-1886-1886");
         FindIdResponse findIdResponse = userService.findId(findIdRequest);
         Assertions.assertThat(findIdResponse.getIdentity()).isEqualTo(null);
@@ -105,8 +95,6 @@ class UserServiceTest {
     @Test
     @DisplayName("비밀번호 찾기 성공 테스트")
     void findPwSuccess() {
-        SignupRequest signupRequest = new SignupRequest("identity", "password", "name", "010-1886-1886", "serena35@ewhain.net");
-        userService.registerUser(signupRequest);
         FindPwRequest findPwRequest = new FindPwRequest("identity", "serena35@ewhain.net");
         FindPwResponse findPwResponse = userService.findPw(findPwRequest);
         Assertions.assertThat(findPwResponse.isEmailSuccess()).isEqualTo(true);
@@ -115,10 +103,24 @@ class UserServiceTest {
     @Test
     @DisplayName("비밀번호 찾기 실패 테스트")
     void findPwFail() {
-        SignupRequest signupRequest = new SignupRequest("identity", "password", "name", "010-1886-1886", "serena35@ewhain.net");
-        userService.registerUser(signupRequest);
         FindPwRequest findPwRequest = new FindPwRequest("identity2", "serena35@ewhain.net");
         FindPwResponse findPwResponse = userService.findPw(findPwRequest);
         Assertions.assertThat(findPwResponse.isEmailSuccess()).isEqualTo(false);
+    }
+
+    @Test
+    @DisplayName("지갑 연결 성공 테스트")
+    void registerWalletSuccess() {
+        RegisterWalletRequest registerWalletRequest = new RegisterWalletRequest(new ObjectId("6231e6bc750b95639ca42496"), "0");
+        RegisterWalletResponse registerWalletResponse = userService.registerWallet(registerWalletRequest);
+        Assertions.assertThat(registerWalletResponse.getRegisterSuccess()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("지갑 연결 실패 테스트")
+    void registerWalletFail() {
+        RegisterWalletRequest registerWalletRequest = new RegisterWalletRequest(new ObjectId(), "0");
+        RegisterWalletResponse registerWalletResponse = userService.registerWallet(registerWalletRequest);
+        Assertions.assertThat(registerWalletResponse.getRegisterSuccess()).isEqualTo(0);
     }
 }
