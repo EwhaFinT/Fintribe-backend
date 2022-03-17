2d037d280b947bbc9f601d6ea80b42739a6ecc70
KimYujeong <70785620+kimyu0218@users.noreply.github.com>
2022-03-17 22:09:21 +0900

    Fix: Art Service 작품 등록 함수 구현 (nft 주소 및 이미지 서버 미완)

--- src/main/java/Fint/FinTribe/service/ArtService.java
+++ src/main/java/Fint/FinTribe/service/ArtService.java
@@ -9,23 +9,31 @@ import org.bson.types.ObjectId;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class ArtService {
     private final ArtRepository artRepository;
 
+    private final AuctionService auctionService;
+
     public UploadResponse upload(UploadRequest uploadRequest) {
         // ==== nft 주소 받기 ====
         // ==== 이미지 서버 등록 ====
+        List<ObjectId> userId = new ArrayList<>();
+        userId.add(uploadRequest.getUserId());
         Art art = Art.builder()
                 .artId(new ObjectId())
                 .artName(uploadRequest.getArtName())
                 .painter(uploadRequest.getPainter())
                 .price(uploadRequest.getPrice())
-                .nftAdd(null).paint(null)
+                .nftAdd(null).paint(null).userId(userId)
                 .detail(uploadRequest.getDetail()).build();
         artRepository.save(art);
+        auctionService.setAuctionDate(art.getArtId(), uploadRequest.getAuctionDate());
         return new UploadResponse(art.getPaint());
     }
 }
