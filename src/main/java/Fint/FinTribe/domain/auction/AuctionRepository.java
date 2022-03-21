a0d86ff2da3ac48e2c2b008d6c578ea2dba7f9c6
KimYujeong <70785620+kimyu0218@users.noreply.github.com>
2022-03-21 23:01:11 +0900

    Feat: Auction Repository 쿼리문 추가

--- src/main/java/Fint/FinTribe/domain/auction/AuctionRepository.java
+++ src/main/java/Fint/FinTribe/domain/auction/AuctionRepository.java
@@ -2,8 +2,13 @@ package Fint.FinTribe.domain.auction;
 
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface AuctionRepository extends MongoRepository<Auction, ObjectId> {
+    @Query("{'isDeleted': ?0}")
+    List<Auction> findByIsDeleted(boolean isDeleted);
 }
