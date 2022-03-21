6e1667c025db3b9930aec01c84c998896fec837b
KimYujeong <70785620+kimyu0218@users.noreply.github.com>
2022-03-22 08:52:18 +0900

    Add: ResaleDate Repository

--- src/main/java/Fint/FinTribe/domain/resaleDate/ResaleDateRepository.java
+++ src/main/java/Fint/FinTribe/domain/resaleDate/ResaleDateRepository.java
@@ -1,2 +1,16 @@
-package Fint.FinTribe.domain.resaleDate;public interface ResaleDateRepository {
+package Fint.FinTribe.domain.resaleDate;
+
+import Fint.FinTribe.domain.auctionDate.AuctionDate;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+@Repository
+public interface ResaleDateRepository extends MongoRepository<ResaleDate, ObjectId> {
+    @Query("{'resaleDate': ?0}")
+    Optional<ResaleDate> findByResaleDate(LocalDate resaleDate);
 }
