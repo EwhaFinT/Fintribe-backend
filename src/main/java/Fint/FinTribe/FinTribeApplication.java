40ffdd554ae5176a27819e08d5db418fcd3ce440
KJI-DEC <kji_dec22@ewhain.net>
2022-04-08 20:36:06 +0900

    Fix: 몽고 설정 삭제

--- src/main/java/Fint/FinTribe/FinTribeApplication.java
+++ src/main/java/Fint/FinTribe/FinTribeApplication.java
@@ -2,13 +2,9 @@ package Fint.FinTribe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
-@EnableMongoRepositories({"Fint.FinTribe.domain"})
+@SpringBootApplication
 @EnableScheduling
 public class FinTribeApplication {
 
