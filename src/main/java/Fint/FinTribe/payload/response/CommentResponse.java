ac0ff78faa242d664d751ef2268f6c53ffaa86f1
KJI-DEC <kji_dec22@ewhain.net>
2022-04-08 20:34:55 +0900

    Fix: getter 추가

--- src/main/java/Fint/FinTribe/payload/response/CommentResponse.java
+++ src/main/java/Fint/FinTribe/payload/response/CommentResponse.java
@@ -1,7 +1,9 @@
 package Fint.FinTribe.payload.response;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
 @AllArgsConstructor
 public class CommentResponse {
     String message;
