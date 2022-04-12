9a80d397e3ca75c7cbdca19ea67a670d34ccc47a
kimyu0218 <serena35@ewhain.net>
2022-04-13 08:59:48 +0900

    service test

--- src/test/java/Fint/FinTribe/service/AuctionServiceTest.java
+++ src/test/java/Fint/FinTribe/service/AuctionServiceTest.java
@@ -13,6 +13,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import xyz.groundx.caver_ext_kas.CaverExtKAS;
+import xyz.groundx.caver_ext_kas.rest_client.io.swagger.client.ApiException;
+import xyz.groundx.caver_ext_kas.rest_client.io.swagger.client.api.wallet.model.*;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -63,7 +66,7 @@ class AuctionServiceTest {
     @DisplayName("새로운 경매 가격 성공 테스트")
     void newPrice() {
         Auction auction = auctions.get(0);
-        NewPriceRequest newPriceRequest = new NewPriceRequest(new ObjectId("62325e0c9ff0d44d0ed82bf0"), auction.getAuctionId(), 100, 1.0, "rlp");
+        NewPriceRequest newPriceRequest = new NewPriceRequest(new ObjectId("62325e0c9ff0d44d0ed82bf0"), auction.getAuctionId(), 100, 1.0, null);
         Assertions.assertThat(auctionService.newPriceSuccess(newPriceRequest));
     }
 
@@ -72,9 +75,9 @@ class AuctionServiceTest {
     void participateAuctionSuccess1() {
         ObjectId userId = new ObjectId("62325e0c9ff0d44d0ed82bf0");
         ObjectId auctionId = new ObjectId("623329392862733ba84fc1d7");
-        NewPriceRequest newPriceRequest = new NewPriceRequest(userId, auctionId, 150, 0.5, "rlp");
+        NewPriceRequest newPriceRequest = new NewPriceRequest(userId, auctionId, 150, 0.5, null);
         ObjectId priceId = auctionService.newPriceSuccess(newPriceRequest).getPriceId();
-        ParticipateAuctionRequest participateAuctionRequest = new ParticipateAuctionRequest(userId, priceId, auctionId, 0.3, "rlp");
+        ParticipateAuctionRequest participateAuctionRequest = new ParticipateAuctionRequest(userId, priceId, auctionId, 0.3, null);
         Assertions.assertThat(auctionService.participateAuctionSuccess(participateAuctionRequest).getPriceId()).isEqualTo(priceId);
     }
 
@@ -84,9 +87,9 @@ class AuctionServiceTest {
         ObjectId userId1 = new ObjectId("62325e0c9ff0d44d0ed82bf0");
         ObjectId userId2 = new ObjectId("6232749829304e715460713e");
         ObjectId auctionId = new ObjectId("623329392862733ba84fc1d7");
-        NewPriceRequest newPriceRequest = new NewPriceRequest(userId1, auctionId, 150, 0.5, "rlp");
+        NewPriceRequest newPriceRequest = new NewPriceRequest(userId1, auctionId, 150, 0.5, null);
         ObjectId priceId = auctionService.newPriceSuccess(newPriceRequest).getPriceId();
-        ParticipateAuctionRequest participateAuctionRequest = new ParticipateAuctionRequest(userId2, priceId, auctionId, 0.5, "rlp");
+        ParticipateAuctionRequest participateAuctionRequest = new ParticipateAuctionRequest(userId2, priceId, auctionId, 0.5, null);
         Assertions.assertThat(auctionService.participateAuctionSuccess(participateAuctionRequest).getPriceId()).isEqualTo(priceId);
     }
 
@@ -105,9 +108,45 @@ class AuctionServiceTest {
     @DisplayName("기존 경매 제안 리스트 & 상한가 가져오기 - 상한가 존재")
     void getPricelist() {
         ObjectId auctionId = new ObjectId("623329392862733ba84fc1d7");
-        PricelistRequest pricelistRequest = new PricelistRequest(auctionId);
-        PricelistResponse pricelistResponse = auctionService.getPricelist(pricelistRequest);
+        PricelistResponse pricelistResponse = auctionService.getPricelist(auctionId);
         List<Price> pricelist = pricelistResponse.getPricelist();
         Assertions.assertThat(pricelistResponse.getPrice()).isNotEqualTo(pricelist.get(0).getAuctionPrice());
+    }
+
+//    (caver.klay랑 caver.kas가 사용하는 문자열 다른지 invalid hex string 출력)
+    /*개발자도구 콘솔에서 다음과 같은 코드 작성 후 트랜잭션 출력
+    const accounts = await window.klaytn.enable()
+    const tx = {
+      type: 'FEE_DELEGATED_SMART_CONTRACT_EXECUTION',
+      from: accounts[0],
+      to: '0x0403d9b7CbF9F0Fac3FECDa84A83B02dF51232BD',
+      data: caver.utils.toPeb('0', 'KLAY'),
+      gas: 21000
+    }
+    const signedTransaction = await caver.klay.signTransaction(tx)
+    const senderRawTransaction = signedTransaction.rawTransaction*/
+
+    @Test
+    @DisplayName("대납 기능 테스트")
+    void Payment() throws ApiException {
+        CaverExtKAS caver = new CaverExtKAS();
+        caver.initKASAPI(1001, "KASK5TJHI4S1SDKGXK4371P0", "iaBvyElV7Ij4ic-iPM-UCyHGirrud7xIuZyuk-fG");
+
+        System.out.println(caver.kas.wallet);
+        String rlp = "0x31f8868085ae9f7bcc00825208940403d9b7cbf9f0fac3fecda84a83b02df51232bd80940403d9b7cbf9f0fac3fecda84a83b02df51232bd80.8f847f8458207f5a0a76e5b3046efd3ad3fb11376915d1ce5a6e27980ed7a63192e057eda20fffc50a00b3c22685f0be624fface7d5ba313224c7cb8e0c508be77baf129651bd1df4af80c4c3018080";
+        FDUserProcessRLPRequest rlpRequest = new FDUserProcessRLPRequest();
+        rlpRequest.setRlp(rlp);
+        rlpRequest.setFeePayer("0x0403d9b7CbF9F0Fac3FECDa84A83B02dF51232BD");
+        rlpRequest.setSubmit(true);
+        System.out.println(rlpRequest);
+
+        try{
+            FDTransactionResult result = caver.kas.wallet.requestFDRawTransactionPaidByUser(rlpRequest);
+            System.out.println(result);
+        }
+        catch(ApiException apiException) {
+            System.out.println(apiException.getResponseBody());
+            apiException.printStackTrace();
+        }
     }
 }
