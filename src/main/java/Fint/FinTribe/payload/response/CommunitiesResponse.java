a2cf78c17b64192a9864c675c89240da981edf39
KJI-DEC <kji_dec22@ewhain.net>
2022-04-13 17:24:01 +0900

    Add: CommunitiesResponse 추가 - communityId를 리스트 형태로 제공

---
+++ src/main/java/Fint/FinTribe/payload/response/CommunitiesResponse.java
@@ -0,0 +1,13 @@
+package Fint.FinTribe.payload.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.bson.types.ObjectId;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class CommunitiesResponse {
+    List<ObjectId> communityIdList;
+}
