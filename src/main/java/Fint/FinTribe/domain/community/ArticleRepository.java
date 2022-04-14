e5dea6332c24374f75d1af251d92e9f37157454f
KJI-DEC <kji_dec22@ewhain.net>
2022-04-14 10:19:27 +0900

    Feat: articleId로 article 받는 메서드 추가

--- src/main/java/Fint/FinTribe/domain/community/ArticleRepository.java
+++ src/main/java/Fint/FinTribe/domain/community/ArticleRepository.java
@@ -5,8 +5,12 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ArticleRepository extends MongoRepository<Article, ObjectId> {
     @Query("{$and:[{'communityId': ?0}, {'isDeleted': ?1}]}")
     List<Article> findByCommunityIdAndIsDeleted(ObjectId communityId,Boolean isDeleted);
+
+    @Query("{$and:[{'articleId': ?0}, {'isDeleted': ?1}]}")
+    Optional<Article> findByArticleIdAndIsDeleted(ObjectId articleId, Boolean isDeleted);
 }
