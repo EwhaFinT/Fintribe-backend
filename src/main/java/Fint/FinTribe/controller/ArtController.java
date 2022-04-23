91fb29ae8ec31aef358b6e5dec3cb4fede84d1e2
kimyu0218 <serena35@ewhain.net>
2022-04-23 16:18:42 +0900

    Feat: artlist, artInfo api 추가

--- src/main/java/Fint/FinTribe/controller/ArtController.java
+++ src/main/java/Fint/FinTribe/controller/ArtController.java
@@ -1,15 +1,15 @@
 package Fint.FinTribe.controller;
 
+import Fint.FinTribe.domain.art.Art;
 import Fint.FinTribe.payload.request.UploadRequest;
+import Fint.FinTribe.payload.response.ArtListResponse;
 import Fint.FinTribe.payload.response.UploadResponse;
 import Fint.FinTribe.service.ArtService;
 import lombok.RequiredArgsConstructor;
+import org.bson.types.ObjectId;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -23,5 +23,19 @@ public class ArtController {
     public ResponseEntity<?> upload(@Valid @RequestBody UploadRequest uploadRequest) {
         UploadResponse uploadResponse = artService.upload(uploadRequest);
         return new ResponseEntity<>(uploadResponse, HttpStatus.OK);
+    }
+
+    // 2. 현재 진행중인 경매의 art url 반환 (artId, paint 리스트)
+    @GetMapping("/artlist")
+    public ResponseEntity<?> getArtList() {
+        ArtListResponse artListResponse = artService.getArtList();
+        return new ResponseEntity<>(artListResponse, HttpStatus.OK);
+    }
+
+    // 3. art 상세 정보 반환
+    @GetMapping("/artInfo")
+    public ResponseEntity<?> getArtInfo(@Valid @RequestParam ObjectId artId) {
+        Art art = artService.getArtInfo(artId);
+        return new ResponseEntity<>(art, HttpStatus.OK);
     }
 }
