81845984d42629d3717ff5164b36533090cd802d
kimyu0218 <serena35@ewhain.net>
2022-04-27 13:59:30 +0900

    Feat: 작품 리스트 조회, 작품 상세 정보 조회 함수 추가

--- src/main/java/Fint/FinTribe/service/ArtService.java
+++ src/main/java/Fint/FinTribe/service/ArtService.java
@@ -2,7 +2,9 @@ package Fint.FinTribe.service;
 
 import Fint.FinTribe.domain.art.Art;
 import Fint.FinTribe.domain.art.ArtRepository;
+import Fint.FinTribe.domain.auction.Auction;
 import Fint.FinTribe.payload.request.UploadRequest;
+import Fint.FinTribe.payload.response.ArtListResponse;
 import Fint.FinTribe.payload.response.UploadResponse;
 import lombok.RequiredArgsConstructor;
 import org.bson.types.ObjectId;
@@ -11,6 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -20,23 +23,44 @@ public class ArtService {
 
     private final AuctionService auctionService;
 
+    // 1. 작품 업로드
     public UploadResponse upload(UploadRequest uploadRequest) {
-        // ==== nft 주소 받기 ====
-        // ==== 이미지 서버 등록 ====
+        // TODO: nft 주소 받기
         List<ObjectId> userId = new ArrayList<>();
         List<Double> ratio = new ArrayList<>();
-        userId.add(uploadRequest.getUserId());
+        userId.add(new ObjectId(uploadRequest.getUserId()));
         ratio.add(1.0);
         Art art = Art.builder()
                 .artId(new ObjectId())
                 .artName(uploadRequest.getArtName())
                 .painter(uploadRequest.getPainter())
                 .price(uploadRequest.getPrice())
-                .nftAdd(null).paint(null).userId(userId).ratio(ratio)
+                .nftAdd(null)
+                .paint(uploadRequest.getPaint()).sold(false)
+                .userId(userId).ratio(ratio)
                 .detail(uploadRequest.getDetail()).build();
         artRepository.save(art);
         auctionService.setAuctionDate(art.getArtId(), uploadRequest.getAuctionDate());
-        return new UploadResponse(art.getPaint());
+        return new UploadResponse(art.getArtId().toString());
+    }
+
+    // 2. 현재 진행중인 경매의 art url 반환 (artId, paint 리스트)
+    public ArtListResponse getArtList() {
+        List<String> artId = new ArrayList<>();
+        List<String> paint = new ArrayList<>();
+
+        List<Auction> auctionList = auctionService.getAuctions();
+        for(int i = 0; i < auctionList.size(); i++) {
+            Optional<Art> art = artRepository.findById(auctionList.get(i).getArtId());
+            artId.add(auctionList.get(i).getArtId().toString());
+            paint.add(art.get().getPaint());
+        }
+        return new ArtListResponse(artId, paint);
+    }
+
+    // 3. art 상세 정보 반환
+    public Art getArtInfo(ObjectId artId) {
+        return artRepository.findById(artId).get();
     }
 
     public Art findArtById(ObjectId artId){
