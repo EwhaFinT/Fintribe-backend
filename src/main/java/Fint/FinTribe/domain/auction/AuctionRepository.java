c4ec49a7ec928e19339d65bb4c53d8938a81820e
kimyu0218 <serena35@ewhain.net>
2022-04-27 13:48:43 +0900

    Feat: findByArtId 쿼리 추가

--- src/main/java/Fint/FinTribe/domain/auction/AuctionRepository.java
+++ src/main/java/Fint/FinTribe/domain/auction/AuctionRepository.java
@@ -6,9 +6,13 @@ import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface AuctionRepository extends MongoRepository<Auction, ObjectId> {
+    @Query("{'artId': ?0}")
+    Optional<Auction> findByArtId(ObjectId artId);
+
     @Query("{'isDeleted': ?0}")
     List<Auction> findByIsDeleted(boolean isDeleted);
 }
