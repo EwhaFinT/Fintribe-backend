edebdee8fad4998f59f77bf05d93f114bdbe8517
KJI-DEC <kji_dec22@ewhain.net>
2022-05-11 17:32:34 +0900

    fix: communityId와 artName 함께 response

--- src/main/java/Fint/FinTribe/payload/response/CommunitiesResponse.java
+++ src/main/java/Fint/FinTribe/payload/response/CommunitiesResponse.java
@@ -2,12 +2,10 @@ package Fint.FinTribe.payload.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import org.bson.types.ObjectId;
-
-import java.util.List;
 
 @Getter
 @AllArgsConstructor
 public class CommunitiesResponse {
-    List<String> communityIdList;
+    String communityId;
+    String artName;
 }
