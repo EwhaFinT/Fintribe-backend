0d998d5604c5269a6823a78c75742ff1d1967dd3
KJI-DEC <kji_dec22@ewhain.net>
2022-05-14 16:41:03 +0900

    feat: add findVotesByEndTime

--- src/main/java/Fint/FinTribe/domain/community/VoteRepository.java
+++ src/main/java/Fint/FinTribe/domain/community/VoteRepository.java
@@ -2,6 +2,12 @@ package Fint.FinTribe.domain.community;
 
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface VoteRepository extends MongoRepository<Vote, ObjectId> {
+    @Query("{'endTime': ?0}")
+    List<Vote> findVotesByEndTime(LocalDateTime endTime);
 }
