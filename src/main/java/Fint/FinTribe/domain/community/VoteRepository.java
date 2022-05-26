53ed86c10e56670272b0a1dfd55ebbe6f940a7d8
KJI-DEC <kji_dec22@ewhain.net>
2022-05-27 00:15:32 +0900

    feat: communityId로 vote 찾는 기능 추가

--- src/main/java/Fint/FinTribe/domain/community/VoteRepository.java
+++ src/main/java/Fint/FinTribe/domain/community/VoteRepository.java
@@ -6,8 +6,11 @@ import org.springframework.data.mongodb.repository.Query;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 public interface VoteRepository extends MongoRepository<Vote, ObjectId> {
     @Query("{'endTime': ?0}")
     List<Vote> findVotesByEndTime(LocalDateTime endTime);
+    @Query("{'communityId': ?0}")
+    Optional<Vote> findVoteByCommunityId(ObjectId communityId);
 }
