57411d82047677252b94a9303c09a43abd0ac68b
KJI-DEC <kji_dec22@ewhain.net>
2022-06-03 15:32:22 +0900

    feat: add findByVoteId

--- src/main/java/Fint/FinTribe/domain/community/ParticipantVoteRepository.java
+++ src/main/java/Fint/FinTribe/domain/community/ParticipantVoteRepository.java
@@ -4,9 +4,12 @@ import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ParticipantVoteRepository extends MongoRepository<ParticipantVote, ObjectId> {
     @Query("{$and:[{'voteId': ?0}, {'userId': ?1}]}")
     Optional<ParticipantVote> findByVoteIdAndUserId(ObjectId voteId, ObjectId userId);
+    @Query("{'voteId': ?0}")
+    List<ParticipantVote> findByVoteId(ObjectId voteId);
 }
